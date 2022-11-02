ddeaa51a693f6f2dba4117c1e487112863fb1cb6
liuhu016 <liuhu016@realsee.com>
2022-11-02 20:05:29 +0800

    fx

--- src/handler/proxy.rs
+++ src/handler/proxy.rs
@@ -1,19 +1,18 @@
 use crate::request::define::Response as APIResponse;
 use crate::request::request::do_request;
+use crate::util::request as util_request;
 
 use axum::{
     async_trait,
-    body::{Bytes, HttpBody},
+    body::{ HttpBody},
     extract::{FromRequest, Path, Query, RequestParts},
-    http::{header::CONTENT_TYPE, StatusCode, Uri, response},
     response::{IntoResponse, Response},
     BoxError,
 };
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
+use serde_json::{ Value};
 use std::{
-    collections::{hash_map::RandomState, HashMap},
-    io::Read,
+    collections::HashMap,
 };
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -36,49 +35,12 @@ where
 
     async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
         let result = Path::from_request(req).await;
-        if result.is_err() {
-            println!("{}", result.as_ref().err().unwrap());
-        }
+
+        let header: HashMap<String, String> = util_request::extract_header(req);
+        let data: Value = util_request::extract_parameter_all(req).await;
         let path_params: Path<Params> = result.unwrap();
         let Path(tmp_params) = path_params;
-        let mut header: HashMap<String, String> = HashMap::new();
-        for (key, value) in req.headers().iter() {
-            let mut tmp_str = String::from("");
-            if value
-                .clone()
-                .as_bytes()
-                .read_to_string(&mut tmp_str)
-                .is_ok()
-            {
-                header.insert(key.to_string(), tmp_str);
-            }
-        }
-        let mut data: Value = json!({});
-        let query: Query<HashMap<String, String, RandomState>> =
-            Query::from_request(req).await.unwrap();
-
-        let object = data.as_object_mut().unwrap();
-        for (key, value) in query.iter() {
-            object.insert(String::from(key), Value::String(value.clone()));
-        }
-
-        let content_type_header = req.headers().get(CONTENT_TYPE);
-        let content_type = content_type_header.and_then(|value| value.to_str().ok());
-
-        if req.method().to_string().eq("POST") {
-            if let Some(content_type) = content_type {
-                if content_type == "application/json" {
-                    let bytes = Bytes::from_request(req).await.unwrap();
-                    let post_data: serde_json::Value = serde_json::from_slice(&bytes).unwrap();
-                    if let Value::Object(post_map) = post_data {
-                        for (key, value) in post_map.iter() {
-                            object.insert(String::from(key), value.clone());
-                        }
-                    }
-                }
-            }
-        }
-
+        
         return Ok(Params {
             params: Some(data),
             api: tmp_params.api,
